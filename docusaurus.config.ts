124798e842e74fecc1301a92240d394e0696bec6
cristian alejandro <calejandro@macbook.pro>
2024-05-16 17:33:43 -0500

    Change baseUrl for deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://crisangera.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/react-native-clean-architecture-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
